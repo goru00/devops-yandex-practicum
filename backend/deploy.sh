5baa6ef94ce08ffe2eebf250a191a8a04ea033b7
goru00 <goru00@vk.com>
2024-09-26 01:40:58 +0300

    change iac

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -6,7 +6,7 @@ sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-back
 sudo rm -f /home/jarservice/sausage-store.jar||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_BACKEND_REPO_DOWNLOAD_ARTIFACT_LINK}
-sudo cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
+sudo cp ./sausage-store.jar /home/student/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
